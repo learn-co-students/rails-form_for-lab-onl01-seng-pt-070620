7360074ee6ac04ff6baeec1dac63372f57beb205
kinnari <kinnari1022@yahoo.com>
2020-11-16 23:29:29 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :school_class, only: [:new]
+  resources :school_classes, only: [:new, :create, :show, :edit, :update]
+  resources :students, only: [:new, :create, :show, :edit, :update]
 end
